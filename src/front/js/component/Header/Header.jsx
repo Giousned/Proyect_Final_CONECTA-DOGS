20156a1b3b36ee593a7481430067a698986fe64c
Giousned <guille_ranza@hotmail.com>
2023-04-19 16:04:15 +0000

    [FIX] botones del header

--- src/front/js/component/Header/Header.jsx
+++ src/front/js/component/Header/Header.jsx
@@ -1,21 +1,23 @@
 import React from "react";
+
+import { Link } from "react-router-dom";
+
 import "../Header/header.css";
 
 export const Header = () => {
+  
   return (
     <header className="d-flex align-items-center custom-header" id="home">
       <div className="container text-white-custom text-shadow text-center">
         <h1 className="mb-4">Conecta-Dogs</h1>
         <h3>Conectamos Cuidadores con Propietarios de Perros</h3>
         <br />
-        {/* <button className="btn-home">Buscar Cuidadores</button>
-        <button className="btn-home">Iniciar Sesión</button> */}
-        <button className="button-home">
+        <Link to="/log-in-form" className="button-home">
           <span className="text">Iniciar Sesión</span>
-        </button>
-        <button className="button-home">
+        </Link>
+        <Link to="/sign-up-form" className="button-home">
           <span className="text">Registrarse</span>
-        </button>
+        </Link>
       </div>
     </header>
   );
