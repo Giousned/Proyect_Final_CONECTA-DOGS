9f65302b547191072847d08e944e30904ddfc892
Sandra Madarnas <sandra.madarnas.1@gmail.com>
2023-05-10 12:33:18 +0000

    Actualizando NavBar - Nombre de Usuario

--- src/front/js/pages/Notifications/Notifications.jsx
+++ src/front/js/pages/Notifications/Notifications.jsx
@@ -9,14 +9,9 @@ const Notifications = () => {
         <div className="row">
           <div className="col-12 my-4">
             <p>
-              <button
-                className="btn btn-primary"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#notificaciones"
-                aria-expanded="false"
-                aria-controls="notificaciones"
-              >
+              <button className="btn btn-primary" type="button"
+                data-bs-toggle="collapse" data-bs-target="#notificaciones"
+                aria-expanded="false" aria-controls="notificaciones">
                 <i className="fas fa-bell"></i> NOTIFICACIONES SIN LEER{" "}
                 <i className="fas fa-bell"></i>
               </button>
@@ -24,38 +19,40 @@ const Notifications = () => {
             <div className="custom-notif-card">
               <div className="collapse collapse-horizontal" id="notificaciones">
                 <div className="card card-body">
-                  <div
-                    className="alert alert-warning alert-dismissible fade show"
-                    role="alert"
-                  >
+                  <div className="alert alert-warning alert-dismissible fade show" role="alert">
                     <strong>¡Tienes una nueva reserva!</strong> Acéptala para
                     que no la pierdas...
-                    <button
-                      type="button"
-                      className="btn-close"
-                      data-bs-dismiss="alert"
-                      aria-label="Close"
-                    ></button>
+                    <button type="button" className="btn-close"
+                      data-bs-dismiss="alert" aria-label="Close">
+                    </button>
                   </div>
-                  <div
-                    className="alert alert-warning alert-dismissible fade show"
-                    role="alert"
-                  >
+                  <div className="alert alert-warning alert-dismissible fade show" role="alert">
                     <strong>¡Tienes listo tu pago!</strong>
-                    <button
-                      type="button"
-                      className="btn-close"
-                      data-bs-dismiss="alert"
-                      aria-label="Close"
+                    <button type="button" className="btn-close"
+                      data-bs-dismiss="alert" aria-label="Close"
                     ></button>
                   </div>
                 </div>
               </div>
             </div>
-            <div></div>
           </div>
+
+
+          <div class="alert alert-primary" role="alert">
+            ¡Tienes una nueva reserva!
+          </div>
+
+          <div class="alert alert-primary" role="alert">
+            ¡Tienes una nueva reserva aceptada!
+          </div>
+
+          <div class="alert alert-danger" role="alert">
+            A simple danger alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
+          </div>
+
         </div>
       </div>
+
     </div>
   );
 };
